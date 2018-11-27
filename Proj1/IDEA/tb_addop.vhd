0bd210ad47d04d871a16934125ca819c1961246a
Karthik Sukumar <karthik.n.sukumar@gmail.com>
2018-11-27 12:31:37 +0100

    Done with the mulop simulation as well need to tweek the tbs for the different values though

--- Proj1/IDEA/tb_addop.vhd
+++ Proj1/IDEA/tb_addop.vhd
@@ -30,66 +30,65 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity tb_addop is
-end tb_addop;
+    end tb_addop;
 
 architecture Behavioral of tb_addop is
 
-	constant c_WIDTH	 :		integer := 16;
-	signal r_ADD_1  : std_logic_vector(c_WIDTH-1 downto 0) := (others => '0');
-	signal r_ADD_2  : std_logic_vector(c_WIDTH-1 downto 0) := (others => '0');
-	signal w_RESULT : std_logic_vector(c_WIDTH downto 0);
-   
-	-- Component Declaration for the Unit Under Test (UUT)
-    COMPONENT addop
-	 GENERIC(
-		BIT_WIDTH	: natural
-		);
-    PORT(
-			I1  	: 	in STD_LOGIC_VECTOR(BIT_WIDTH - 1 downto 0);
-			I2  	: 	in STD_LOGIC_VECTOR(BIT_WIDTH - 1 downto 0);
-			O 		: 	out STD_LOGIC_VECTOR(BIT_WIDTH downto 0));
-    END COMPONENT addop;
-    
-   --Inputs
-   --signal CLK,I1,I2 : std_logic := '0';
+    constant c_WIDTH	 :		integer := 16;
+    signal r_ADD_1  : std_logic_vector(c_WIDTH-1 downto 0) := (others => '0');
+    signal r_ADD_2  : std_logic_vector(c_WIDTH-1 downto 0) := (others => '0');
+    signal w_RESULT : std_logic_vector(c_WIDTH downto 0);
 
- 	--Outputs
-   --signal O : std_logic;
-   -- No clocks detected in port list. Replace <clock> below with 
-   -- appropriate port name 
-    
-	 signal Tb_Clk      : std_logic := '0';
-    constant  Tb_I1	: INTEGER := 313;
+    -- Component Declaration for the Unit Under Test (UUT)
+    COMPONENT addop
+        GENERIC(
+                   BIT_WIDTH	: natural
+               );
+        PORT(
+                I1  	: 	in STD_LOGIC_VECTOR(BIT_WIDTH - 1 downto 0);
+                I2  	: 	in STD_LOGIC_VECTOR(BIT_WIDTH - 1 downto 0);
+                O 		: 	out STD_LOGIC_VECTOR(BIT_WIDTH downto 0));
+    END COMPONENT addop;
+
+     --Inputs
+     --signal CLK,I1,I2 : std_logic := '0';
+
+     --Outputs
+     --signal O : std_logic;
+     -- No clocks detected in port list. Replace <clock> below with 
+     -- appropriate port name 
+
+    signal Tb_Clk      : std_logic := '0';
+    constant  Tb_I1	: INTEGER := 512;
     constant Tb_I2	: INTEGER := 512;
 
-	constant  Tb_I1_1	: INTEGER := 0;
-	constant Tb_I2_2	: INTEGER := 4;
-	
-	-- Clk period
-   constant clk_period : time := 10 ns;
+    constant  Tb_I1_1	: INTEGER := 0;
+    constant Tb_I2_2	: INTEGER := 4;
+
+   -- Clk period
+    constant clk_period : time := 10 ns;
 
 BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
-   uut: addop generic map (
-		BIT_WIDTH	=> c_WIDTH 
-	)
-			port map(
-			 I1	=> r_XOR_1,
-			 I2	=> r_XOR_2,
-			 O		=> w_RESULT
-			 );
-			 
-   I_process : process
-   BEGIN
-       Tb_Clk <= '0';
-			wait for clk_period/2;
-            r_ADD_1 <= std_logic_vector(to_unsigned(Tb_I1,r_ADD_1'length));
-            r_ADD_2 <= std_logic_vector(to_unsigned(Tb_I2,r_ADD_2'length));
-       Tb_Clk <= '1';
-			wait for clk_period/2;
-            r_ADD_1 <= std_logic_vector(to_unsigned(Tb_I1_1,r_ADD_1'length));
-            r_ADD_2 <= std_logic_vector(to_unsigned(Tb_I2_2,r_ADD_2'length));
-   end process;
-	
+
+   -- Instantiate the Unit Under Test (UUT)
+    uut: addop generic map (
+                               BIT_WIDTH	=> c_WIDTH
+                           )
+    port map(
+                I1	=> r_ADD_1,
+                I2	=> r_ADD_2,
+                O		=> w_RESULT);
+
+    I_process : process
+    BEGIN
+        Tb_Clk <= '0';
+        wait for clk_period/2;
+        r_ADD_1 <= std_logic_vector(to_unsigned(Tb_I1,r_ADD_1'length));
+        r_ADD_2 <= std_logic_vector(to_unsigned(Tb_I2,r_ADD_2'length));
+        Tb_Clk <= '1';
+        wait for clk_period/2;
+        r_ADD_1 <= std_logic_vector(to_unsigned(Tb_I1_1,r_ADD_1'length));
+        r_ADD_2 <= std_logic_vector(to_unsigned(Tb_I2_2,r_ADD_2'length));
+    end process;
+
 end Behavioral;
--- Proj1/IDEA/tb_mulop.vhd
+++ Proj1/IDEA/tb_mulop.vhd
@@ -34,8 +34,51 @@ end tb_mulop;
 
 architecture Behavioral of tb_mulop is
 
+    constant c_WIDTH	 :		integer := 16;
+    signal r_MUL_1  : std_logic_vector(c_WIDTH - 1 downto 0) := (others => '0');
+    signal r_MUL_2  : std_logic_vector(c_WIDTH - 1 downto 0) := (others => '0');
+    signal w_RESULT : std_logic_vector(c_WIDTH - 1 downto 0);
+
+    COMPONENT mulop
+        GENERIC(
+                    BIT_WIDTH   : natural
+               );
+        PORT(
+             I1 : in STD_LOGIC_VECTOR(BIT_WIDTH - 1 downto 0);
+             I2 : in STD_LOGIC_VECTOR(BIT_WIDTH - 1 downto 0);
+             O : out STD_LOGIC_VECTOR(BIT_WIDTH - 1 downto 0)
+            );
+    END COMPONENT mulop;
+
+    signal Tb_Clk       : STD_LOGIC := '0';
+    constant Tb_I1      : STD_LOGIC_VECTOR(c_WIDTH - 1 downto 0) := x"0000";
+    constant Tb_I2      : STD_LOGIC_VECTOR(c_WIDTH - 1 downto 0) := x"0000";
+    constant Tb_I1_1    : STD_LOGIC_VECTOR(c_WIDTH - 1 downto 0) := x"0000";
+    constant Tb_I2_2    : STD_LOGIC_VECTOR(c_WIDTH - 1 downto 0) := x"0001";
+
+   -- Clk period
+    constant clk_period : time := 10 ns;
 begin
 
+   -- Instantiate the Unit Under Test (UUT)
+    uut: mulop generic map (
+                               BIT_WIDTH	=> c_WIDTH
+                           )
+    port map(
+                I1  => r_MUL_1,
+                I2  => r_MUL_2,
+                O   => w_RESULT);
 
+    M_process : process
+    begin
+        Tb_Clk <= '0';
+        wait for clk_period/2;
+            r_MUL_1 <= Tb_I1;
+            r_MUL_2 <= Tb_I2;
+        Tb_Clk <= '1';
+        wait for clk_period/2;
+            r_MUL_1 <= Tb_I1_1;
+            r_MUL_2 <= Tb_I2_2;
+    end process M_process;
 end Behavioral;
 
